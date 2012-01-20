c21a76e6745312566c6a2775cfff0c8b728c024e
Tomás Touceda <chiiph@torproject.org>
2012-01-20 14:57:30 -0300

    Add a SkipVersionCheck config option for Tails

--- src/vidalia/MainWindow.cpp
+++ src/vidalia/MainWindow.cpp
@@ -1655,6 +1655,9 @@ MainWindow::circuitEstablished()
 void
 MainWindow::checkTorVersion()
 {
+  VidaliaSettings settings;
+  if(settings.skipVersionCheck())
+    return;
   QString status;
   if (_torControl->getInfo("status/version/current", status)) {
     if (!status.compare("old", Qt::CaseInsensitive)
